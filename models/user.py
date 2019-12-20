42bd6a087a4fc6d7839244ce2c1d48e815cc073b
Liliana327 <894@holbertonschool.com>
2019-12-20 22:24:01 +0000

    add

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This is the user class"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String
+import os
 
 
 class User(BaseModel):
@@ -13,7 +14,14 @@ class User(BaseModel):
         last_name: last name
     """
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = name = Column(String(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
