dd811d80c51025a8331b8b5f223e0bf99bff2ef6
Liliana327 <894@holbertonschool.com>
2019-12-21 05:37:54 +0000

    Actualizacinnn User: ( models/user.py)

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the user class"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from models.place import Place
@@ -15,10 +16,9 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = 'users'
-
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
