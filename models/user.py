edc05436dc1b338d53fa3954d704f8c10be103f3
Liliana327 <894@holbertonschool.com>
2019-12-21 05:28:39 +0000

    Actualizacinnn User: ( models/user.py)

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-import os
+from sqlalchemy import Column, String
+from models.place import Place
 from sqlalchemy.orm import relationship
 
 
@@ -16,15 +16,9 @@ class User(BaseModel, Base):
     """
     __tablename__ = 'users'
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="user", cascade="delete")
-        reviews = relationship("Review", backref="user", cascade="delete")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
