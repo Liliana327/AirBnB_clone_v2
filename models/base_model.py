550e47326a4d092e2ee8855536230ded089d1601
02KellyV <900@holbertonschool.com>
2019-12-21 01:37:53 +0000

    user

--- models/base_model.py
+++ models/base_model.py
@@ -73,6 +73,10 @@ class BaseModel:
             returns a dictionary of all the key values in __dict__
         """
         my_dict = dict(self.__dict__)
+        try:
+            del my_dict['_sa_instance_state']
+        except KeyError:
+            pass
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
