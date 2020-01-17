cc4991dd96838f6ed40b388489e7d6fa0fdec8f5
Liliana327 <894@holbertonschool.com>
2020-01-17 02:43:12 +0000

    modifaile

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,29 +4,27 @@ that distributes an archive to your web servers
 using the function do_deploy: '''
 
 from fabric.api import put, run, env
-import os.path
-env.hosts = ['35.196.101.171', '35.237.255.234	']
+from os.path import exists
+env.hosts = ['35.196.101.171', '35.237.255.234']
 
 
 def do_deploy(archive_path):
     '''Deploy archive'''
-    if os.path.isfile(archive_path) is False:
+    if exists(archive_path) is False:
         return False
     try:
         filename = archive_path.split("/")[-1]
         not_ext = filename.split(".")[0]
         not_path = "/data/web_static/releases/{}/".format(not_ext)
-        symlink = "/data/web_static/current"
+        symlink = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(not_path))
-        run("tar -xzf /tmp/{} -C {}".format(filename, not_path))
+        run("mkdir -p {}{}".format(not_path, not_ext))
+        run("tar -xzf /tmp/{} -C {}{}".format(filename, not_path, not_ext))
         run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(not_path, not_path))
-        run("rm -rf {}web_static".format(not_path))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(not_path, symlink))
-        if result.failed:
-            return False
+        run("mv {0}{1}/web_static/* {0}{1}/".format(not_path, not_ext))
+        run("rm -rf {}{}web_static".format(not_path, not_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s  {}{}/ /data/web_static/current".format(not_path, not_ext))
         return True
     except:
         return False
