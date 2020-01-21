9ddd6706d4b6cc3a9fe294b177fb06b81d8eee47
Liliana327 <894@holbertonschool.com>
2020-01-21 20:55:41 +0000

    /python/(<text>):  followed by the value of the text variable (replace underscore _ symbols with a space )Python display

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+''' script that starts a Flask web application
+'''
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_route():
+    """print hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    """print HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def C_text(text):
+    """print /c/<text>"""
+    replace_text = text.replace("_", " ")
+    return 'C {}'.format(replace_text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """print python(text)"""
+    replace_ptext = text.replace("_", " ")
+    return 'Python {}'.format(replace_ptext)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
